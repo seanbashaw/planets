5d5ba2c1d4a07ca36943783cf20510e352bf41be
seanbashaw <seanbshw@gmail.com>
2015-03-24 16:36:54 -0700

    Added a circle to the screen, made sure it was visible. Circle should be moving towards the left.

--- PlanetStimulator/src/Planet.java
+++ PlanetStimulator/src/Planet.java
@@ -1,12 +1,23 @@
+import org.lwjgl.opengl.GL11;
 
 public class Planet {
-public float x;
-public float y;
-public float size;
-public Planet(float x, float y, float size){
-	this.x=x;
-	this.y=y;
-	this.size=size;
+	public float x;
+	public float y;
+	public float radius;
+	public double[] color = {Math.random(),Math.random(),Math.random()};
+	public Planet(float x, float y, float size) {
+		this.x = x;
+		this.y = y;
+		this.radius = size;
+	}
 
-}
+	public void drawCircle() {
+		GL11.glColor3d(color[0], color[1], color[2]);
+		GL11.glBegin(GL11.GL_TRIANGLE_FAN);
+		for (int i = 0; i < 360; i++) {
+			float angle = (float) ((Math.PI * i) / 180);
+			GL11.glVertex2d(Math.cos(angle) * radius+x, Math.sin(angle) * radius+y);
+		}
+		GL11.glEnd();
+	}
 }
--- PlanetStimulator/src/View.java
+++ PlanetStimulator/src/View.java
@@ -1,9 +1,11 @@
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 
 public class View {
+	public Planet sun = new Planet(400,300,50);
 	public View(){
 		
 	}
@@ -15,10 +17,21 @@ public void start(){
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
+    GL11.glMatrixMode(GL11.GL_PROJECTION);
+    GL11.glLoadIdentity();
+    GL11.glOrtho(0, 600, 0, 600, 1, -1);
+    GL11.glMatrixMode(GL11.GL_MODELVIEW);
 	while (!Display.isCloseRequested()){
+		draw();
 		Display.update();
+		Display.sync(100);
 	}
 	Display.destroy();
+}
+private void draw() {
+	GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);  
+	sun.drawCircle();
+	sun.x++;
 }
 public static void main(String[] args){
 	View view = new View();
